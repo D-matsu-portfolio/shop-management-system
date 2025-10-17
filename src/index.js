b003c7769c4a6c1277ca99ae59aa34fec26fa2eb
syuttyoseibi <daishi2matsuda@gmail.com>
2025-10-17 09:22:01 +0900

    Fix: Correct server catch-all      route and remove path dependency

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../frontend/build')));
 
   // Handles any requests that don't match the ones above
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
   });
 } else {
